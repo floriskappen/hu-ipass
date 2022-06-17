af73ca546c39c90c06265801cc6969886f193b52
Floris Kappen <kappenfloris@gmail.com>
2022-06-17 15:13:52 +0200

    fix: edit data_preprocessing step to use new file structure

--- data_preprocessing/extract_mfcc_librosa.py
+++ data_preprocessing/extract_mfcc_librosa.py
@@ -16,40 +16,62 @@ HOP_LENGTH = 512
 
 def extract_mfcc():
     genres = [name for name in os.listdir(DATA_DIR)]
-    
-    # Dictionary to store data
-    data = {
+
+    data_template = {
         "mapping": genres,
         "mfcc": [],
         "labels": []
     }
+    datas = {
+        "train": data_template.copy(),
+        "test": data_template.copy(),
+        "validation": data_template.copy(),
+    }
+
 
     for index, genre in enumerate(genres):
-        genre_data_dir = DATA_DIR + genre + "/cut/"
-        for video_id in os.listdir(genre_data_dir):
-            video_cuts_dir = genre_data_dir + f"{video_id}/"
-            for video_cut in os.listdir(video_cuts_dir):
-                video_cut_path = Path(video_cuts_dir + video_cut).absolute()
-                with wave.open(str(video_cut_path), "rb") as wave_file:
-                    frame_rate = wave_file.getframerate()
-                signal, sr = librosa.load(video_cut_path, sr=frame_rate)
-                for segment in range(NUM_SEGMENTS):
-                    num_samples_per_segment = int(frame_rate / NUM_SEGMENTS)
-                    start_sample = num_samples_per_segment * segment # s=0 -> 0
-                    finish_sample = start_sample + num_samples_per_segment # s=0 -> num_samples_per_segment
-                    expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment / HOP_LENGTH)
-                    mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
-                                                sr=sr,
-                                                n_fft=2048,
-                                                n_mfcc=13,
-                                                hop_length=512)
-                    mfcc = mfcc.T
-                    if len(mfcc) == expected_num_mfcc_vectors_per_segment:
-                        data["mfcc"].append(mfcc.tolist())
-                        data["labels"].append(index)
+        set_dirs = {
+            "train": DATA_DIR + genre + "/cut/train/",
+            "test": DATA_DIR + genre + "/cut/test/",
+            "validation": DATA_DIR + genre + "/cut/validation/",
+        }
+        for set_type, set_dir in set_dirs.items():
+            # Dictionary to store data
+            data = {
+                "mapping": genres,
+                "mfcc": [],
+                "labels": []
+            }
 
-    with open("../extracted_mfccs.json", "w") as f:
-        f.write(json.dumps(data))
+            for video_id in os.listdir(set_dir):
+                video_cuts_dir = set_dir + f"{video_id}/"
+                for video_cut in os.listdir(video_cuts_dir):
+                    audio_cut_path = Path(video_cuts_dir + video_cut).absolute()
+                    try:
+                        with wave.open(str(audio_cut_path), "rb") as wave_file:
+                            frame_rate = wave_file.getframerate()
+                        signal, sr = librosa.load(audio_cut_path, sr=frame_rate)
+                        for segment in range(NUM_SEGMENTS):
+                            num_samples_per_segment = int(frame_rate / NUM_SEGMENTS)
+                            start_sample = num_samples_per_segment * segment # s=0 -> 0
+                            finish_sample = start_sample + num_samples_per_segment # s=0 -> num_samples_per_segment
+                            expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment / HOP_LENGTH)
+                            mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
+                                                        sr=sr,
+                                                        n_fft=2048,
+                                                        n_mfcc=13,
+                                                        hop_length=512)
+                            mfcc = mfcc.T
+                            if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+                                datas[set_type]["mfcc"].append(mfcc.tolist())
+                                datas[set_type]["labels"].append(index)
+                    except wave.Error as e:
+                        print(e)
+                        continue
+
+    for data_type in datas:
+        with open(f"{DATA_DIR}extracted_mfccs_{data_type}.json", "w") as f:
+            f.write(json.dumps(datas[data_type]))
 
 if __name__ == "__main__":
     extract_mfcc()
